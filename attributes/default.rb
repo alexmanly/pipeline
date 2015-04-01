5c02891758b3b4f440d7cc34062bfd4aed02aa6e
alexmanly <alexmanly@gmail.com>
2015-04-01 06:05:07 -0700

    modified chef repo url

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default['pipeline']['chef_server']['url'] = "http://0.0.0.0:80"
 default['pipeline']['chef_server']['node_name'] = "pipeline"
 
 # use example chef-repo and poll master branch every minute by default
-default['pipeline']['chef-repo']['url'] = "https://github.com/stephenlauck/pipeline-example-chef-repo.git"
+default['pipeline']['chef-repo']['url'] = "https://github.com/alexmanly/chef-repo.git"
 default['pipeline']['chef-repo']['branch'] = "*/master"
 default['pipeline']['chef-repo']['polling'] = "* * * * *"
 
